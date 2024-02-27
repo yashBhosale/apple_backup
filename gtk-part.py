7bd0cd020bc4c2fdf59750ec5c20323d5b913b63
Yash Bhosale <bhosaley5@gmail.com>
2024-02-27 02:05:45 -0600

    making it work without workbench

--- gtk-part.py
+++ gtk-part.py
@@ -2,28 +2,48 @@ import gi
 
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
-from gi.repository import Gtk, Adw
-import workbench
+from gi.repository import Gtk, Adw, Gio, Gdk
+import sys
+
+@Gtk.Template(filename="./blueprint.xml")
+class Window1(Adw.ApplicationWindow):
+    __gtype_name__ = "window1"
+
+    @Gtk.Template.Callback()
+    def dialog_response(parent, data):
+        print(parent, data)
+        data("hello")
+        return 
+
+        dialog = Gtk.FileDialog()
+
+        def select_finish(dialog, result):
+            try:
+                folder = dialog.select_folder_finish(result)
+                data(folder.get_path())
+
+            except Gtk.DialogError:
+                pass
+
+        dialog.select_folder(None, None, select_finish)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        print(Gtk.Template.Child())
+    
+
+def on_activate(app):
+    win = Window1()
+    win.present()
+    provider = Gtk.CssProvider()
+
+    provider.load_from_path("./css-part.css")
+    Gtk.StyleContext.add_provider_for_display(Gdk.Display.get_default(), provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+    win.set_application(app)
 
 
-def dialog_response(parent, data):
-    dialog = Gtk.FileDialog()
-
-    def select_finish(dialog, result):
-        try:
-            folder = dialog.select_folder_finish(result)
-            data(folder.get_path())
-
-        except Gtk.DialogError:
-            pass
-
-    dialog.select_folder(None, None, select_finish)
-
-
-button: Gtk.Box = workbench.builder.get_object("folder-find-button")
-file_text = workbench.builder.get_object("file-text")
-
-button.connect("clicked", dialog_response, lambda path: file_text.set_text(path))
-
-
+app = Adw.Application()
+app.connect('activate', on_activate)
+exit_status= app.run(sys.argv)
+sys.exit(exit_status)
 
